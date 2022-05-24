a7c680b62d91c5760388a54fa170282bec95ad71
SONGDAM <herb7761@gmail.com>
2022-05-24 13:39:51 +0900

    feat: Look components

--- src/components/Look.js
+++ src/components/Look.js
@@ -1,15 +1,51 @@
 import { ImageStateContext } from '../routes/Home';
-import { useContext, useState } from 'react';
+import { weatherStateContext } from '../routes/Home';
+import { useContext, useState, useEffect } from 'react';
 
 function Look() {
   const lookList = ['All', 'casual', 'modern', 'street', 'romantic'];
   const images = useContext(ImageStateContext);
+  const temp = useContext(weatherStateContext);
   const [imgArray, setImgArray] = useState([]);
+  const [nowTemp, setNowTemp] = useState(17);
   const handleImages = Object.values(images);
+
+  useEffect(() => {
+    if (temp >= 23 && temp < 28) {
+      setNowTemp(23);
+      return;
+    }
+    if (temp >= 20 && temp <= 22) {
+      setNowTemp(20);
+      return;
+    }
+    if (temp >= 17 && temp <= 19) {
+      setNowTemp(17);
+      return;
+    }
+    if (temp >= 12 && temp <= 16) {
+      setNowTemp(12);
+      return;
+    }
+    if (temp >= 9 && temp <= 11) {
+      setNowTemp(9);
+      return;
+    }
+    if (temp >= 5 && temp <= 8) {
+      setNowTemp(5);
+      return;
+    }
+    if (temp <= 4) {
+      setNowTemp(4);
+      return;
+    }
+  }, [temp]);
 
   const onClick = (e) => {
     const lookName = e.target.textContent;
-    const result = handleImages.filter((image) => image.look === lookName);
+    const result = handleImages.filter(
+      (image) => image.look === lookName && image.temperature === nowTemp
+    );
     setImgArray(result);
   };
 
