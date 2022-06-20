b8dd1af6f6225a70b5b676cf17f908e1a1898afb
NagyeongHa <hnk3176@naver.com>
2022-06-20 21:59:47 +0900

    chore: 좋아요 코드 정리

--- src/components/Like.js
+++ src/components/Like.js
@@ -37,7 +37,7 @@ function Like({ imageDummy }) {
         const user = Object.values(snapshot.val()); //유저이메일
         //로그인했을 때 이미지별 좋아요 눌린 유저중에 로그인 유저랑 같은 사람이 있는지
         if (authUser) {
-          if (user.filter((item) => item.user === authUser.email)[0]) {
+          if (user.find((item) => item.user === authUser.email)) {
             setisLike(true);
           }
         }
@@ -62,12 +62,6 @@ function Like({ imageDummy }) {
   //좋아요
   const upLike = () => {
     setisLike(true);
-    console.log('업');
-
-    //카운트+1
-    update(getCountReference, {
-      count: viewNumber.count + 1,
-    });
 
     //likes안에 저장될 고유키 생성
     const newLikeKey = push(child(ref(database), `likes`)).key;
@@ -82,24 +76,22 @@ function Like({ imageDummy }) {
     const updates = {};
     updates[`/database/look/${imageIndex}/likes/` + newLikeKey] = likeData;
     update(ref(database), updates);
+
+    //카운트+1
+    update(getCountReference, {
+      count: viewNumber.count + 1,
+    });
   };
 
   //좋아요 취소
   const downLike = () => {
     setisLike(false);
-    console.log('다운');
-
-    //카운트-1
-    update(getCountReference, {
-      count: viewNumber.count - 1,
-    });
 
     //유저제거
     if (viewNumber.likes) {
       const toArray = Object.values(viewNumber.likes);
       const userFilter = toArray.filter((item) => item.user === authUser.email);
       const likesUuid = userFilter[0].uuid;
-      // console.log('좋아요 취소 키', likesUuid);
 
       const removeUserReference = ref(
         database,
@@ -107,6 +99,11 @@ function Like({ imageDummy }) {
       );
 
       remove(removeUserReference);
+
+      //카운트-1
+      update(getCountReference, {
+        count: viewNumber.count - 1,
+      });
     }
   };
 
