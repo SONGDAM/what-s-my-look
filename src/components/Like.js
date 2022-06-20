cd58472de9b42cd2050f32c77d5e53d361172007
NagyeongHa <hnk3176@naver.com>
2022-06-20 21:48:37 +0900

    fix: 좋아요 파이어베이스 오류 진짜 수정

--- src/components/Like.js
+++ src/components/Like.js
@@ -2,7 +2,7 @@ import unLike from '../assets/icon/empty-heart.png';
 import like from '../assets/icon/full-heart.png';
 import '../styles/like.css';
 import { useState, useEffect } from 'react';
-import { update, ref, onValue, remove, push, child } from 'firebase/database';
+import { update, ref, onValue, push, child, remove } from 'firebase/database';
 import { useRecoilValue } from 'recoil';
 import { authState } from '../recoil/authState';
 import { database } from './firebase';
@@ -37,7 +37,7 @@ function Like({ imageDummy }) {
         const user = Object.values(snapshot.val()); //유저이메일
         //로그인했을 때 이미지별 좋아요 눌린 유저중에 로그인 유저랑 같은 사람이 있는지
         if (authUser) {
-          if (user.map((item) => item.user === authUser.email)) {
+          if (user.filter((item) => item.user === authUser.email)[0]) {
             setisLike(true);
           }
         }
@@ -54,16 +54,14 @@ function Like({ imageDummy }) {
 
     if (isLike) {
       downLike();
+      return;
     }
-    if (!isLike) {
-      upLike();
-    }
+    upLike();
   };
 
   //좋아요
   const upLike = () => {
     setisLike(true);
-
     console.log('업');
 
     //카운트+1
@@ -74,6 +72,7 @@ function Like({ imageDummy }) {
     //likes안에 저장될 고유키 생성
     const newLikeKey = push(child(ref(database), `likes`)).key;
 
+    //저장할 값 ( 이메일 , 고유키)
     const likeData = {
       user: authUser.email,
       uuid: newLikeKey,
@@ -90,23 +89,6 @@ function Like({ imageDummy }) {
     setisLike(false);
     console.log('다운');
 
-    //유저제거
-    // onValue(getLikesUserReference, (snapshot) => {
-    //   if (snapshot.exists()) {
-    //     const values = Object.values(snapshot.val()).filter(
-    //       (item) => item.user === authUser.email
-    //     );
-    //     const key = values.map((item) => item.uuid);
-    //     console.log('key is', key);
-
-    //     const removeUserReference = ref(
-    //       database,
-    //       `database/look/${imageIndex}/likes/${key}`
-    //     );
-    //    remove(removeUserReference);
-    //   }
-    // });
-
     //카운트-1
     update(getCountReference, {
       count: viewNumber.count - 1,
@@ -114,13 +96,14 @@ function Like({ imageDummy }) {
 
     //유저제거
     if (viewNumber.likes) {
-      const matchUser = Object.values(viewNumber.likes);
-      const likesKey = matchUser[0].uuid;
-      console.log('좋아요 취소 키', likesKey);
+      const toArray = Object.values(viewNumber.likes);
+      const userFilter = toArray.filter((item) => item.user === authUser.email);
+      const likesUuid = userFilter[0].uuid;
+      // console.log('좋아요 취소 키', likesUuid);
 
       const removeUserReference = ref(
         database,
-        `database/look/${imageIndex}/likes/${likesKey}`
+        `database/look/${imageIndex}/likes/${likesUuid}`
       );
 
       remove(removeUserReference);
