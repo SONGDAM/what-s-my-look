ecb11f6b3ea482a7ee8623848a4eedefadf7046b
SONGDAM <herb7761@gmail.com>
2022-07-01 01:40:13 +0900

    test ci/cd with github actions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Liked from './routes/Liked';
 import { Suspense } from 'react';
 import Loading from './components/Loading';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <RecoilRoot>
       <Suspense fallback={<Loading />}>
         <Routes>
@@ -16,6 +16,6 @@ ReactDOM.render(
         </Routes>
       </Suspense>
     </RecoilRoot>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
