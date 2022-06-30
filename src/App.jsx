ea2beac0d30fd84300fc05e64358dc5d6d6727d7
SONGDAM <herb7761@gmail.com>
2022-07-01 00:49:31 +0900

    test ci/cd with github actions

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
   //여기서 유저검증 && 파이어베이스 토큰 체크하고 조건부 라우팅 하면 될듯
 
   return (
-    <Suspense fallback={<Loading />}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/liked' element={<Liked />} />
         </Routes>
-      </BrowserRouter>
-    </Suspense>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
